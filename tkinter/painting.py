d5bf8d5c0de9570255d95cdda1b773c3256d3b16
VangolaXXI <samidin_1989@mail.ru>
2023-10-27 15:44:46 +0600

    Добавлен треугольник как фигура

--- tkinter/painting.py
+++ tkinter/painting.py
@@ -37,12 +37,14 @@ class SimplePaint:
         circle_button = ttk.Button(self.control_frame, text="Круг", command=lambda: self.set_mode("circle"))
         circle_button.pack(fill=tk.X, pady=5)
 
+        triangle_button = ttk.Button(self.control_frame, text="Треугольник", command=lambda: self.set_mode("triangle"))
+        triangle_button.pack(fill=tk.X, pady=5)
+
         clear_button = ttk.Button(self.control_frame, text="Очистить", command=self.clear_canvas)
         clear_button.pack(fill=tk.X, pady=5)
 
         color_button = ttk.Button(self.control_frame, text="Выбрать цвет", command=self.change_color)
         color_button.pack(fill=tk.X, pady=5)
-       
 
         self.root.grid_rowconfigure(0, weight=1)
         self.root.grid_columnconfigure(0, weight=1)
@@ -52,8 +54,6 @@ class SimplePaint:
 
     def set_mode(self, mode):
         self.mode = mode
-
-    
 
     def on_canvas_click(self, event):
         self.prev_x = event.x
@@ -66,11 +66,11 @@ class SimplePaint:
                 self.canvas.create_line(self.prev_x, self.prev_y, x, y, fill=self.pen_color, width=self.brush_size)
                 self.prev_x = x
                 self.prev_y = y
-        elif self.mode in ["square", "rectangle", "circle"]:
+        elif self.mode in ["square", "rectangle", "circle", "triangle"]:
             self.preview_shape(x, y)
 
     def on_canvas_release(self, event):
-        if self.mode in ["square", "rectangle", "circle"] and self.drawing:
+        if self.mode in ["square", "rectangle", "circle", "triangle"] and self.drawing:
             self.finalize_shape(event.x, event.y)
 
     def preview_shape(self, x, y):
@@ -78,12 +78,32 @@ class SimplePaint:
             self.canvas.delete(self.drawing)
         if self.mode == "square":
             side = max(abs(x - self.prev_x), abs(y - self.prev_y))
-            self.drawing = self.canvas.create_rectangle(self.prev_x, self.prev_y, x, y, outline=self.pen_color, fill=self.fill_color)
+            if x < self.prev_x:
+                side = -side
+            if y < self.prev_y:
+                self.drawing = self.canvas.create_rectangle(self.prev_x, self.prev_y-side, self.prev_x-side, self.prev_y, outline=self.pen_color, fill=self.fill_color)
+            else:
+                self.drawing = self.canvas.create_rectangle(self.prev_x, self.prev_y, self.prev_x+side, self.prev_y+side, outline=self.pen_color, fill=self.fill_color)
+
         elif self.mode == "rectangle":
             self.drawing = self.canvas.create_rectangle(self.prev_x, self.prev_y, x, y, outline=self.pen_color, fill=self.fill_color)
         elif self.mode == "circle":
             r = ((x - self.prev_x) ** 2 + (y - self.prev_y) ** 2) ** 0.5
             self.drawing = self.canvas.create_oval(self.prev_x - r, self.prev_y - r, self.prev_x + r, self.prev_y + r, outline=self.pen_color, fill=self.fill_color)
+        elif self.mode == "triangle":
+            self.draw_triangle(x, y)
+
+    def draw_triangle(self, x, y):
+        if self.drawing:
+            self.canvas.delete(self.drawing)
+        middle_x = (self.prev_x + x) / 2
+        self.drawing = self.canvas.create_polygon(
+            self.prev_x, self.prev_y,
+            middle_x, y,
+            x, self.prev_y,
+            outline=self.pen_color,
+            fill=self.fill_color
+        )
 
     def finalize_shape(self, x, y):
         self.preview_shape(x, y)
